b309fe4afec15f0986dd7c204a47546a035372dc
jasoot <78027764+jasoot@users.noreply.github.com>
2021-02-01 20:07:49 +0200

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+;int main(int argc, char** argv) 
+{
+	float a,b,c;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"a+b="<<c;
+	return 0;
+}
